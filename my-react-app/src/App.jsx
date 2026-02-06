706352ecb918b4043fe9b06fe4b8d1f6bff40721
DominikCarreon <dvcarreon@student.apc.edu.ph>
2026-02-06 10:42:46 +0800

    Added text

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,6 +2,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World! ILOVEYOU 69,000 Everyday</h1>
+      <h1> Ano ang makukuha mo sa baboy na magaling mag-karate?</h1>
+      <h1>Eh 'di... PORK CHOP!</h1>
     </div>
   );
 }
